e7f18921f264793ccde8a2a9cf639ca4d8d32148
Andy Swanson <swan1625@icloud.com>
2022-01-16 13:45:41 -0600

    Added 6 albums to array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+var collection = []
+
+function addToCollection(title, artist, yearPublished){
+ let a = title + artist + yearPublished;
+ collection.push(a)
+ return a;
+}
+
+console.log( 'Adding to collection:', addToCollection( 'Are you Experienced?,', ' Jimi Hendrix,', ' 1967' ));
+console.log( 'Adding to collection:', addToCollection( 'Innerspeaker,', ' Tame Impala,', ' 2009' ));
+console.log( 'Adding to collection:', addToCollection( 'Digital Ash In a Digital Urn,', ' Bright Eyes,', ' 2005' ));
+console.log( 'Adding to collection:', addToCollection( 'Shame, Shame, ', 'Dr. Dog,', ' 2010' ));
+console.log( 'Adding to collection:', addToCollection( 'Strange Mercy,', ' St. Vincent,', ' 2011' ));
+console.log( 'Adding to collection:', addToCollection( 'Rocket,', ' Alex G,', ' 2017' ));
+console.log(collection);

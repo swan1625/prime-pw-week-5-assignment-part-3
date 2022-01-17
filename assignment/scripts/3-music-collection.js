1582a35ea024c5105ac59f2383ad1d47736d981d
Andy Swanson <swan1625@icloud.com>
2022-01-17 14:49:52 -0600

    Still working on find by artist and show collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,19 +8,33 @@ function addToCollection(title, artist, yearPublished){
  return a;
 }
 
-console.log( 'Adding to collection:', addToCollection( 'Are you Experienced?,', ' Jimi Hendrix,', ' 1967' ));
-console.log( 'Adding to collection:', addToCollection( 'Innerspeaker,', ' Tame Impala,', ' 2009' ));
-console.log( 'Adding to collection:', addToCollection( 'Digital Ash In a Digital Urn,', ' Bright Eyes,', ' 2005' ));
-console.log( 'Adding to collection:', addToCollection( 'Shame, Shame, ', 'Dr. Dog,', ' 2010' ));
-console.log( 'Adding to collection:', addToCollection( 'Strange Mercy,', ' St. Vincent,', ' 2011' ));
-console.log( 'Adding to collection:', addToCollection( 'Rocket,', ' Alex G,', ' 2017' ));
+console.log( 'Adding to collection:', addToCollection( 'Are you Experienced?', 'Jimi Hendrix', '1967' ));
+console.log( 'Adding to collection:', addToCollection( 'Innerspeaker', 'Tame Impala', ' 2009' ));
+console.log( 'Adding to collection:', addToCollection( 'Digital Ash In a Digital Urn', 'Bright Eyes', '2005' ));
+console.log( 'Adding to collection:', addToCollection( 'Shame, Shame', 'Dr. Dog', '2010' ));
+console.log( 'Adding to collection:', addToCollection( 'Strange Mercy', ' St. Vincent', '2011' ));
+console.log( 'Adding to collection:', addToCollection( 'Rocket', ' Alex G', ' 2017' ));
 
 
 
 console.log(collection);
 
 function showCollection(array) {
-  console.log(array)
+  console.log(array);
 }
 
 console.log(showCollection(collection));
+
+
+function findByArtist(artist){
+  let findByArtist = [];
+  for ( let i = 0; i<collection.length; i++){
+      if (collection[i].artist === artist){
+        findByArtist.push(collection[i].artist);
+      }
+    }
+console.log(findByArtist);
+
+}
+
+findByArtist('Jimi Hendrix');
